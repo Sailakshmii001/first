7f4c50468acd89a778b9d8e9e4d4a621e3a99ead
sailakshmii001 <nagasailakshmi001@gmail.com>
2024-01-23 16:10:09 +0530

    first project

---
+++ and.java
@@ -0,0 +1,7 @@
+class _76
+{
+public static void main(String[]args)
+{
+System.out.println(true||true);
+}
+}
---
+++ apples.java
@@ -0,0 +1,9 @@
+class _76
+{
+public static void main(String[]args)
+{
+int apples=5;
+int oranges=7;
+System.out.println(apples==oranges);
+}
+}
